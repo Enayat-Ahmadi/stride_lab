1f0f240ecf12ab556416cd3ca3e7552a4428085d
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-13 12:38:58 +0100

    optimize code to prevent the possible crashes

--- pages/cart.js
+++ pages/cart.js
@@ -2,21 +2,21 @@ import useCart from "@/hooks/useCart";
 import CartProductCard from "@/components/Cart/CartProductCard";
 
 export default function Cart({ products }) {
-  const {
-    productCart,
-    removeFromCart,
-    increaseQuantity,
-    decreaseQuantity,
-  } = useCart();
+  const { productCart, removeFromCart, increaseQuantity, decreaseQuantity } =
+    useCart();
 
-  const cartProducts = productCart.map((item) => {
-    const product = products.find((product) => product._id === item.id);
-    return {
-      ...product,
-      quantity: item.quantity,
-    };
-  });
-  console.log(cartProducts);
+  const cartProducts =
+    productCart
+      .map((item) => {
+        const product = products.find((product) => product._id === item.id);
+        if (!product) return null;
+        return {
+          ...product,
+          quantity: item.quantity,
+        };
+      })
+      .filter(Boolean) || [];
+
   return (
     <div className="min-h-screen p-5">
       {cartProducts.map((product) => (
