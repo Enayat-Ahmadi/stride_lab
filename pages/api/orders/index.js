6bc9c32460340e51a70276880ddd721faad70544
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-18 09:49:02 +0100

    fix order api bug and improve it:

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -3,18 +3,18 @@ import Order from "@/db/models/Order";
 
 export default async function handler(req, res) {
   await dbConnect();
-  if (req.method === "POST") {
-    try {
-      const newOrder = await req.body;
-      await Order.create({ newOrder });
-      res.status(201).json({ status: "Order created successfully" });
-      return;
-    } catch (error) {
-      console.error;
-      res.status(400).json({ status: error.message });
-      return;
-    }
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
   }
-  res.status(405).json({ status: "Method not allowed" });
-  return;
+  try {
+    const newOrder = req.body;
+    const order = await Order.create(newOrder);
+    res.status(201).json({ status: "Order created successfully", order });
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ status: error.message });
+    return;
+  }
 }
