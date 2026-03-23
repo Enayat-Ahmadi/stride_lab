34a2b80e157b504b3fa90cb3ece86c02df08ec3d
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-23 16:24:55 +0100

    redirect to details page when click on the search result

--- components/SearchOverly.jsx
+++ components/SearchOverly.jsx
@@ -23,7 +23,9 @@ export default function SearchOverly({ products }) {
       },
     });
   }
-
+  function handleProductClick(productId) {
+    router.push(`/${productId}`);
+  }
   return (
     <div className="mx-auto max-w-2xl">
       <form onSubmit={handleSerach} className="relative flex">
@@ -34,12 +36,12 @@ export default function SearchOverly({ products }) {
           placeholder="search product..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="w-full rounded-full border px-4 py-3 text-lg outline-none"
+          className="w-full h-10 rounded-full border px-4 py-3 text-lg outline-none"
         />
         <button
           type="submit"
           variant="ghost"
-          className="absolute right-2 top-1"
+          className="absolute right-2 top-2"
         >
           <Search />
         </button>
@@ -50,6 +52,7 @@ export default function SearchOverly({ products }) {
             <button
               key={product._id}
               type="button"
+              onClick={() => handleProductClick(product._id)}
               className="block w-full rounded-xl border p-3 text-left hover:bg-neutral-100"
             >
               <p className="font-medium">{product.name}</p>
