ac763006385914aa1ede3abe3e9829c1ed089d0c
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-25 18:18:01 +0100

    fetch orders from db

--- pages/admin/orders/index.js
+++ pages/admin/orders/index.js
@@ -1,7 +1,17 @@
+import useSWR from "swr";
+import ErrorScreen from "@/components/ui/ErrorScreen";
+import LoadingScreen from "@/components/ui/LoadingScreen";
+
+
 export default function Orders() {
+  const { data, error, isLoading } = useSWR("/api/orders");
+  if (error) return <ErrorScreen />;
+  if (isLoading) return <LoadingScreen />;
+  console.log(data);
+  const orders = Array.isArray(data) ? data : [];
   return (
-    <main>
-      <h1>Orders page</h1>
+    <main className="max-w-6xl min-h-screen flex flex-col mx-auto gap-3 bg-background px-4 py-8 md:px-8">
+
     </main>
   );
 }
